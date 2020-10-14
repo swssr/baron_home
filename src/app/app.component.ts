84b8bb9b0d7a9d93f545a29ea44cdc61d8019833
Simo <simocmo4@gmail.com>
2020-10-14 07:25:13 +0200

    Clean html and map data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = "CodeSandbox";
+
+  private campaigns: any = [
+    {
+      title: "wednesday",
+      src:
+        "https://res.cloudinary.com/tumi/image/upload/v1602648200/Home%20PWAs/Baron/compressed/BD_BARON_RIB_WEDNESDAY_In_app_awarness_screen_1000_x_7000.jpg"
+    },
+    {
+      title: "tuesday",
+      src:
+        "https://res.cloudinary.com/tumi/image/upload/v1602649488/Home%20PWAs/Baron/compressed/BD_BARON_STEAK_TUESDAY_In_app_awarness_screen_1000_x_7000.jpg"
+    },
+    {
+      title: "ladies",
+      src:
+        "https://res.cloudinary.com/tumi/image/upload/v1602649573/Home%20PWAs/Baron/compressed/BD_BARON_LADIES_LUNCH_In_app_awarness_screen_1000_x_7000.jpg"
+    }
+  ];
+  private current: any = null;
+
+  ngOnInit(): void {}
+
+  openCampaign(title) {
+    console.log(title);
+  }
 }
